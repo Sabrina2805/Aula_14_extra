be715fe5fcb5428f085fe4def89ad8db656f0e51
sabrina2805 <sabrinamouradias@gmail.com>
2026-09-21 10:46:17 -0300

    dados e script - Etapa 2

--- script_roteiro_aula_14_extra.R
+++ script_roteiro_aula_14_extra.R
@@ -185,3 +185,5 @@ BACO_AULA14_RJ <- bind_rows(df_uf, df_mun)
 # Tarefa 5: Exportar o banco de dados BANCO_AULA14_RJ com o nome BANCO_AULA14_RJ.csv
 
 # Ao terminar a Tarefa 5 commit com a mensagem "dados e script - Etapa 2" e envie para o repositório Aula_14_Extra
+
+write.csv(BACO_AULA14_RJ, file = "BANCO_AULA14_RJ.csv", row.names = FALSE)
